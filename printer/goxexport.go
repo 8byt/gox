ce263c7255a16587a3e2238504306f29cf0c8f3c
Eric Miller <eric@legoaces.org>
2017-06-10 00:55:07 -0700

    Added hypothetical output of AST for non-Component objects

--- printer/goxexport.go
+++ printer/goxexport.go
@@ -1,26 +1,49 @@
 package printer
 
 import (
-	"fmt"
-	"strings"
+	"strconv"
+
+	"unicode"
 
 	"github.com/8byt/gox/ast"
+	"github.com/8byt/gox/token"
 )
 
-func goxToVecty(gox *ast.GoxExpr) string {
-	args := []string{gox.TagName.Name}
+func goxToVecty(gox *ast.GoxExpr) ast.Node {
+	isComponent := unicode.IsUpper(gox.TagName.Name[0])
 
-	for _, attr := range gox.Attrs{
-		var str string
-		switch attr.Rhs.(type){
-		case ast.GoExpr:
-			str = 
+	if isComponent {
+		return ast.NewIdent("COMPONENT")
+	} else {
+		args := []ast.Expr{
+			ast.BasicLit{
+				Kind:  token.STRING,
+				Value: strconv.Quote(gox.TagName.Name),
+			}}
 
+		// Add the attributes
+		for _, attr := range gox.Attrs {
+			expr := ast.CallExpr{
+				Fun: ast.SelectorExpr{X: ast.NewIdent("vecty"), Sel: ast.NewIdent("Attribute")},
+				Args: []ast.Expr{
+					ast.BasicLit{Kind: token.STRING,
+						Value: strconv.Quote(attr.Lhs.Name)},
+					attr.Rhs,
+				},
+			}
 
+			args = append(args, expr)
 		}
-		str := fmt.Sprintf()
-		args = append(args, )
-	}
 
-	return fmt.Sprintf(`vecty.Tag("%s")`, strings.Join(args, ", "))
+		// Add the contents
+		for _, expr := range gox.X {
+			args = append(args, expr)
+		}
+
+		selector := ast.SelectorExpr{
+			X:   ast.NewIdent("vecty"),
+			Sel: ast.NewIdent("Tag")}
+
+		return ast.CallExpr{Fun: selector, Args: args}
+	}
 }
